af185f0c16eea59890193e265725061356a9691f
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-09-16 16:53:28 -0400

    New product handler

--- handlers/schedule.go
+++ handlers/schedule.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/context"
 	"github.com/heynickc/fpc/dal"
@@ -30,4 +32,27 @@ func GetSchedule(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.Execute(w, data)
+}
+
+func PostProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	db := context.Get(r, "db").(*sqlx.DB)
+
+	name := r.FormValue("Name")
+	roastDate, err := time.Parse("Monday Jan 02, 2006", r.FormValue("RoastDate"))
+	description := r.FormValue("Description")
+	price, err := strconv.ParseFloat(r.FormValue("Price"), 64)
+	if err != nil {
+		libhttp.HandleErrorJson(w, err)
+		return
+	}
+
+	_, err = dal.NewProduct(db).InsertNewProduct(nil, name, description, roastDate, price)
+	if err != nil {
+		libhttp.HandleErrorJson(w, err)
+		return
+	}
+
+	http.Redirect(w, r, "/schedule", 302)
 }
